384a4c89483e5fb8e2ba71e70b6ca09fab3672e5
Simran Singh Rathore <33891350+justsimransingh@users.noreply.github.com>
2019-07-17 16:16:44 +0530

    Add files via upload

---
+++ task20.java
@@ -0,0 +1,14 @@
+//Time take by program to execute
+class task20
+{
+	public static void main(String [] args)
+	{
+		long start_time=System.nanoTime();
+		//code start
+		System.out.println("Hello! Welcome to the JAVA world...");
+		//code end
+		long end_time=System.nanoTime();
+		System.out.println("Time taken (in nano seconds)");
+		System.out.println(end_time-start_time);
+	}
+}
