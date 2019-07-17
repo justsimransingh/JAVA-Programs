1b862ff555423b39f6286360926d8c9fd1e8d48f
Simran Singh Rathore <33891350+justsimransingh@users.noreply.github.com>
2019-07-17 18:19:08 +0530

    Update task11.java

--- task11.java
+++ task11.java
@@ -4,23 +4,31 @@ class task11
 {
 public static void main(String [] args)
 {
-int n1n,i;
+int n,i;
 System.out.println("Enter two numbers");
 int n1=new Scanner(System.in).nextInt();
 int n2=new Scanner(System.in).nextInt();
 System.out.println("Prime numbers are");
-for(n1n=n1+1;n1n<n2;n1n++)
+for(n=n1+1;n<n2;n++)
 {
-for(i=2;i<n1n;i++)
+for(i=2;i<=n;i++)
 {
-if(n1n%i==0)
+if(n%i==0)
 {
 break;
 }
-else if(i==n1n)
+else
 {
-System.out.println(n1n);
+continue;
 }
+}
+if(i==n)
+{
+System.out.println(n);
+}
+else
+{
+continue;
 }
 }
 }
