5d50d6bc262fed73797920b50093f3ebfd9941e7
Simran Singh Rathore <33891350+justsimransingh@users.noreply.github.com>
2019-07-17 16:23:33 +0530

    Update task10.java

--- task10.java
+++ task10.java
@@ -4,9 +4,10 @@ class task10
 {
 	public static void main(String [] args)
 	{
-		int temp=0;
+		int temp=0,x;
 		System.out.println("Enter a number");
 		int num = new Scanner(System.in).nextInt();
+		x=num;
 		for(int i=2;i<num;i++)
 		{
 			if(num%i==0)
@@ -16,9 +17,13 @@ class task10
 				break;
 			}
 		}
-		if(temp==0)
+		if(temp==0 && x>1)
 		{
 			System.out.println("Number is prime");
+		}
+		else
+		{
+			System.out.println("Number is not prime");
 		}
 	}
 }
